2d021baf2f7981062eb73195f8d3b96d60634ccc
Igor Zvyagin <spirit-drive@yandex.ru>
2021-07-30 20:56:28 +0300

    made a checkout

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,19 @@
-console.log(1);
+/* eslint-disable */
+import { Trans } from './Trans';
+// import ru from './lang_ru.json';
+// import en from './lang_en.json';
+
+enum Locale {
+  ru = 'ru',
+  en = 'en',
+}
+
+const trans = new Trans<Locale>();
+
+(async () => {
+  // await trans.init({ translations: { ru, en }, locale: Locale.en });
+  await trans.init({ translations: { ru: import('./lang_ru.json'), en: import('./lang_en.json') }, locale: Locale.en });
+  console.log(trans, { ...trans.content });
+  await trans.changeLocale(Locale.ru);
+  console.log(trans, { ...trans.content });
+})();
