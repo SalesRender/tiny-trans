63800467f2d87e51e950513b80110a365cac35d3
Igor Zvyagin <spirit-drive@yandex.ru>
2021-08-13 23:56:43 +0300

    preparing for npm 6

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,60 @@
-export { Trans } from '../src/core/Trans';
-export * from '../src/core/types';
+export class ContentNotFound extends Error {}
+export class ContentNotPlural extends Error {}
+export class PluralNotSupport extends Error {}
+export class InvalidTranslate extends Error {}
+export class InvalidPath extends Error {}
+export class InvalidErrorMode extends Error {}
+
+export type TransError = InvalidTranslate | ContentNotFound | ContentNotPlural;
+
+export type Content = Record<string, unknown>;
+export type DynamicContent<T extends Content> = () => Promise<{ default: T }>;
+export type Variables = Record<string, string>;
+export type ErrorsMode = 'ignore' | 'throw' | ((error: TransError) => string);
+export type PluralContent = Partial<Record<Intl.LDMLPluralRule, string>>;
+export type PluralFn = (count: number, locale: string) => Intl.LDMLPluralRule;
+export enum Config {
+  count = 'COUNT',
+}
+
+export type TranslateOptions<T extends Variables = Variables> = {
+  /**
+   * ignore the error, handle error or throw error (by default)
+   * */
+  errorsMode?: ErrorsMode;
+  /**
+   * it uses for the plural if it exists
+   * */
+  count?: number;
+  /**
+   * for replaced the variable patters - ${variable}
+   * */
+  variables?: T;
+};
+
+export type Translate<T extends Variables = Variables> = (
+  path: string | TemplateStringsArray,
+  options?: TranslateOptions<T>
+) => string;
+
+export declare class Trans<Locale extends string = string> {
+  locale: Locale;
+
+  content: Content;
+
+  init<T extends Content>(params: {
+    translations: Record<Locale, T>;
+    locale: Locale;
+    pluralRecord?: Record<Locale, PluralFn>;
+  }): Promise<void>;
+
+  init<T extends Content>(params: {
+    translations: Record<Locale, DynamicContent<T>>;
+    locale: Locale;
+    pluralRecord?: Record<Locale, PluralFn>;
+  }): Promise<void>;
+
+  changeLocale(locale: Locale): Promise<void>;
+
+  createTranslate<T extends Variables = Variables>(module: string | TemplateStringsArray): Translate<T>;
+}
